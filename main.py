dcc52a2c92ca027ce82a2aea2dfa469e658ad3a4
dkarasevgthub <k4rasev.mitya@ya.ru>
2020-12-26 13:09:20 +0300

    Отрисовка блоков, и стен на поле игры

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ WIDTH = 400
 HEIGHT = 600
 FPS = 60
 GRAVITY = 0.25
+JUMP_HEIGHT = 75  # временно, использовал для создания блоков, когда игрока будешь делать поменяй, блоки подстроятся
 
 pygame.display.set_caption('Once upon a castle')
 pygame.display.set_icon(pygame.image.load('data/player.png'))
@@ -19,6 +20,7 @@ static_block = pygame.sprite.Group()
 dynamic_block = pygame.sprite.Group()
 start_sprites = pygame.sprite.Group()
 crashed_block = pygame.sprite.Group()
+wall = pygame.sprite.Group()
 player = pygame.sprite.Group()
 
 
@@ -77,6 +79,7 @@ class StaticBlock(pygame.sprite.Sprite):
         self.image = pygame.transform.scale(load_image('static_block.png'), (60, 20))
         self.rect = self.image.get_rect()
         self.rect.x, self.rect.y = x, y
+        self.rect.h = 6
 
 
 class DynamicBlock(pygame.sprite.Sprite):
@@ -85,11 +88,12 @@ class DynamicBlock(pygame.sprite.Sprite):
         self.image = pygame.transform.scale(load_image('dynamic_block.png'), (60, 20))
         self.rect = self.image.get_rect()
         self.rect.x, self.rect.y = x, y
+        self.rect.h = 6
 
     def update(self):
         if self.rect.x >= 25 and self.rect.x + self.rect.w <= WIDTH - 25:
             self.rect = self.rect.move(1, 0)
-        else:
+        elif self.rect.x != 25:
             pass
 
 
@@ -99,13 +103,66 @@ class CrashedBlock(pygame.sprite.Sprite):
         self.image = pygame.transform.scale(load_image('crashed_block.png'), (60, 20))
         self.rect = self.image.get_rect()
         self.rect.x, self.rect.y = x, y
-        self.velocity = [random.choice(range(-5, 5)), random.choice(range(-5, 5))]
-        self.gravity = GRAVITY
-        self.screen_rect = (0, 0, WIDTH, HEIGHT)
+        self.rect.h = 6
 
     def update(self):
-        # BlocksDropping()
-        pass
+        if pygame.sprite.spritecollideany(self, player):
+            self.kill()
+            CrashedBlockRight((self.rect.x, self.rect.y))
+            CrashedBlockLeft((self.rect.x, self.rect.y))
+
+
+class CrashedBlockRight(pygame.sprite.Sprite):
+    def __init__(self, pos):
+        super().__init__(crashed_block)
+        self.image = pygame.transform.scale(load_image('crashed_block_right.png'), (60, 20))
+        self.rect = self.image.get_rect()
+        self.screen = (0, 0, WIDTH, HEIGHT)
+        self.velocity = [1, -1]
+        self.rect.x, self.rect.y = pos
+        self.gravity = GRAVITY
+
+    def update(self):
+        self.velocity[1] += self.gravity
+        self.rect.x += self.velocity[0]
+        self.rect.y += self.velocity[1]
+        if not self.rect.colliderect(self.screen):
+            self.kill()
+
+
+class CrashedBlockLeft(pygame.sprite.Sprite):
+    def __init__(self, pos):
+        super().__init__(crashed_block)
+        self.image = pygame.transform.scale(load_image('crashed_block_left.png'), (60, 20))
+        self.rect = self.image.get_rect()
+        self.screen = (0, 0, WIDTH, HEIGHT)
+        self.velocity = [-1, -1]
+        self.rect.x, self.rect.y = pos
+        self.gravity = GRAVITY
+
+    def update(self):
+        self.velocity[1] += self.gravity
+        self.rect.x += self.velocity[0]
+        self.rect.y += self.velocity[1]
+        if not self.rect.colliderect(self.screen):
+            self.kill()
+
+
+class WallLeft(pygame.sprite.Sprite):
+    def __init__(self, pos):
+        super().__init__(wall)
+        self.image = pygame.transform.scale(load_image('wall.png'), (35, 35))
+        self.rect = self.image.get_rect()
+        self.rect.x, self.rect.y = pos
+
+
+class WallRight(pygame.sprite.Sprite):
+    def __init__(self, pos):
+        super().__init__(wall)
+        self.image = pygame.transform.rotate(
+            pygame.transform.scale(load_image('wall.png'), (35, 35)), 180)
+        self.rect = self.image.get_rect()
+        self.rect.x, self.rect.y = pos
 
 
 class Camera:
@@ -124,22 +181,32 @@ class Camera:
 
 start_screen()
 running = True
+for i in range(WIDTH // 18):
+    WallLeft((-(35 // 2), 35 * i))
+for i in range(WIDTH // 18):
+    WallRight((WIDTH - (35 // 2), 35 * i))
+for i in range(0, HEIGHT, JUMP_HEIGHT):
+    if i // JUMP_HEIGHT == random.randint(1, HEIGHT // JUMP_HEIGHT):
+        DynamicBlock(random.randint(25, WIDTH - 85), i)
+    elif i // JUMP_HEIGHT == random.randint(1, HEIGHT // JUMP_HEIGHT):
+        CrashedBlock(random.randint(25, WIDTH - 85), i)
+    else:
+        StaticBlock(random.randint(25, WIDTH - 85), i)
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
-            DynamicBlock(event.pos[0], event.pos[1])
+            StaticBlock(event.pos[0], event.pos[1])
     background = pygame.transform.scale(load_image('background.png'), (WIDTH, HEIGHT))
     screen.blit(background, (0, 0))
-    wall = pygame.transform.scale(load_image('wall.png'), (35, 35))
-    for i in range(WIDTH // 18):
-        screen.blit(wall, (-(wall.get_width() // 2), wall.get_height() * i))
-    wall = pygame.transform.rotate(wall, 180)
-    for i in range(WIDTH // 18):
-        screen.blit(wall, (WIDTH - (wall.get_width() // 2), wall.get_height() * i))
+    wall.draw(screen)
+    static_block.draw(screen)
     dynamic_block.draw(screen)
     dynamic_block.update()
+    crashed_block.draw(screen)
+    crashed_block.update()
     pygame.display.flip()
     clock.tick(FPS)
 terminate()
