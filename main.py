0cc677aa2725b16148749957bb5d74f7ee550a2b
dkarasevgthub <k4rasev.mitya@ya.ru>
2020-12-28 00:15:17 +0300

    working at start screen and end screen + some fixes

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import random
 import sys
+
 import pygame
 
 pygame.init()
@@ -18,6 +19,8 @@ clock = pygame.time.Clock()
 static_block = pygame.sprite.Group()
 dynamic_block = pygame.sprite.Group()
 start_sprites = pygame.sprite.Group()
+end_player = pygame.sprite.Group()
+end_sprites = pygame.sprite.Group()
 crashed_block = pygame.sprite.Group()
 wall = pygame.sprite.Group()
 start_player = pygame.sprite.Group()
@@ -40,52 +43,70 @@ def load_image(name, colorkey=None):
     return image
 
 
-def start_screen():
-    # задний фон
+def game():
+    score = 0
     background = pygame.transform.scale(load_image('background.png'), (WIDTH, HEIGHT))
-    screen.blit(background, (0, 0))
-    # текст
-    font = pygame.font.Font('data/font.ttf', 20)
-    text = font.render("Have fun!", True, pygame.Color((20, 20, 20)))
-    text_rect = text.get_rect()
-    text_rect.x = 30
-    text_rect.y = 90
-    screen.blit(text, text_rect)
-    # блок
-    block = pygame.sprite.Sprite(start_sprites)
-    block.image = pygame.transform.scale(load_image('static_block.png'), (75, 25))
-    block.rect = block.image.get_rect()
-    block.rect.x, block.rect.y = 250, 450
-    # игрок
-    StartScreenPlayer((block.rect.x, block.rect.y))
-    # основной цикл
-    while True:
+    for sprite in static_block:
+        sprite.kill()
+    for sprite in dynamic_block:
+        sprite.kill()
+    for sprite in crashed_block:
+        sprite.kill()
+    running = True
+    for i in range(WIDTH // 18):
+        WallLeft((-(35 // 2), 35 * i))
+    for i in range(WIDTH // 18):
+        WallRight((WIDTH - (35 // 2), 35 * i))
+    for i in range(0, HEIGHT - JUMP_HEIGHT // 2, JUMP_HEIGHT // 2):
+        if i // (JUMP_HEIGHT // 2) == random.randint(1, HEIGHT // (JUMP_HEIGHT // 2)):
+            DynamicBlock(random.randint(25, WIDTH - 85), i)
+        elif i // (JUMP_HEIGHT // 2) == random.randint(1, HEIGHT // (JUMP_HEIGHT // 2)):
+            CrashedBlock(random.randint(25, WIDTH - 85), i)
+        else:
+            StaticBlock(random.randint(25, WIDTH - 85), i)
+    first_block_pos_x = random.randint(25, WIDTH - 85)
+    StaticBlock(first_block_pos_x, HEIGHT - JUMP_HEIGHT // 2)
+    hero = Player((first_block_pos_x, (HEIGHT - JUMP_HEIGHT // 2) - 55))
+    while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                terminate()
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                return
-        start_sprites.draw(screen)
-        start_player.draw(screen)
-        start_player.update()
+                running = False
+        pressed = pygame.key.get_pressed()
+        if pressed[pygame.K_RIGHT]:
+            hero.right(5)
+        elif pressed[pygame.K_LEFT]:
+            hero.left(5)
+        screen.blit(background, (0, 0))
+        wall.draw(screen)
+        static_block.draw(screen)
+        dynamic_block.draw(screen)
+        dynamic_block.update()
+        crashed_block.draw(screen)
+        crashed_block.update()
+        player.draw(screen)
+        player.update()
         pygame.display.flip()
         clock.tick(FPS)
-        screen.blit(background, (0, 0))
-        screen.blit(text, text_rect)
+    terminate()
 
 
-def terminate():
-    pygame.quit()
-    sys.exit()
+def score_screen():
+    pass
+
+
+def save_score():
+    pass
 
 
 class StartScreenPlayer(pygame.sprite.Sprite):
-    def __init__(self, block_pos):
-        super().__init__(start_player)
+    def __init__(self, player_pos, type='start'):
+        if type == 'end':
+            super().__init__(end_player)
+        else:
+            super().__init__(start_player)
         self.image = pygame.transform.scale(load_image('player.png'), (55, 55))
         self.rect = self.image.get_rect()
-        self.rect.x, self.rect.y = 255, 300
-        self.block_pos = block_pos
+        self.rect.x, self.rect.y = player_pos
         self.velocity = [0, 0]
         self.gravity = GRAVITY
         self.fl = True
@@ -104,6 +125,155 @@ class StartScreenPlayer(pygame.sprite.Sprite):
             self.velocity[1] -= self.gravity
             self.rect.x -= self.velocity[0]
             self.rect.y -= self.velocity[1]
+
+
+StartScreenPlayer((255, 300))
+
+
+def start_screen():
+    # задний фон
+    background = pygame.transform.scale(load_image('background.png'), (WIDTH, HEIGHT))
+    screen.blit(background, (0, 0))
+    # текст
+    font = pygame.font.Font('data/font.ttf', 20)
+    text = font.render("Have fun!", True, pygame.Color((20, 20, 20)))
+    text_rect = text.get_rect()
+    text_rect.x = 30
+    text_rect.y = 90
+    screen.blit(text, text_rect)
+    # блок
+    block = pygame.sprite.Sprite(start_sprites)
+    block.image = pygame.transform.scale(load_image('static_block.png'), (75, 25))
+    block.rect = block.image.get_rect()
+    block.rect.x, block.rect.y = 250, 450
+    # кнопки
+    start_image = pygame.transform.scale(load_image('start.png'), (140, 60))
+    btn_rect = start_image.get_rect()
+    btn_rect.x, btn_rect.y = 50, 380
+    exit_image = pygame.transform.scale(load_image('exit.png'), (140, 50))
+    exit_image_rect = exit_image.get_rect()
+    exit_image_rect.x, exit_image_rect.y = btn_rect.x, btn_rect.y + btn_rect.h + 10
+    score_tab_image = pygame.transform.scale(load_image('score_tab.png'), (140, 50))
+    score_tab_image_rect = score_tab_image.get_rect()
+    score_tab_image_rect.x, score_tab_image_rect.y = btn_rect.x, btn_rect.y + btn_rect.h + 10 + score_tab_image_rect.h + 5
+    # основной цикл
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if 50 <= event.pos[0] <= 190 and event.pos[1] >= 380 and \
+                        event.pos[1] <= 440:
+                    game()
+                if event.pos[0] >= 50 and event.pos[0] <= 190 and btn_rect.y + btn_rect.h + 10 <= \
+                        event.pos[
+                            1] <= btn_rect.y + btn_rect.h + 60:
+                    exit()
+                if event.pos[0] >= 50 and event.pos[
+                    0] <= 190 and btn_rect.y + btn_rect.h + 15 + score_tab_image_rect.h <= \
+                        event.pos[
+                            1] <= btn_rect.y + btn_rect.h + 15 + score_tab_image_rect.h + 50:
+                    score_screen()
+        screen.blit(background, (0, 0))
+        screen.blit(text, text_rect)
+        screen.blit(start_image, btn_rect)
+        screen.blit(exit_image, exit_image_rect)
+        screen.blit(score_tab_image, score_tab_image_rect)
+        start_sprites.draw(screen)
+        start_player.draw(screen)
+        start_player.update()
+        pygame.display.flip()
+        clock.tick(FPS)
+
+
+StartScreenPlayer((295, 300), 'end')
+
+
+def game_over_screen(score):
+    # очки
+    font = pygame.font.Font('data/font.ttf', 16)
+    text = font.render('Score: ' + str(score) + ' points', True, pygame.Color((20, 20, 20)))
+    text_rect = text.get_rect()
+    text_rect.x, text_rect.y = 30, 100
+    # фон
+    background = pygame.transform.scale(load_image('background.png'), (WIDTH, HEIGHT))
+    # кнопки и картинки
+    save_image = pygame.transform.scale(load_image('save_score.png'),
+                                        (text_rect.h + 10, text_rect.h + 10))
+    save_image_rect = save_image.get_rect()
+    save_image_rect.x, save_image_rect.y = text_rect.x + text_rect.w + 15, text_rect.y - 5
+    game_over_image = pygame.transform.scale(load_image('game_over.png'), (240, 120))
+    image_rect = game_over_image.get_rect()
+    image_rect.x, image_rect.y = WIDTH // 2 - image_rect.w // 2, HEIGHT // 2 - image_rect.h
+    restart_image = pygame.transform.scale(load_image('restart.png'), (140, 60))
+    restart_image_rect = restart_image.get_rect()
+    restart_image_rect.x, restart_image_rect.y = WIDTH // 2 - restart_image_rect.w // 2 - \
+                                                 15, HEIGHT // 2 - restart_image_rect.h // 2 + 125
+    exit_image = pygame.transform.scale(load_image('exit.png'), (110, 42))
+    exit_image_rect = exit_image.get_rect()
+    exit_image_rect.x, exit_image_rect.y = WIDTH // 2 - exit_image_rect.w // 2, HEIGHT // 2 - \
+                                           exit_image_rect.h // 2 + 125 + restart_image_rect.h + 5
+    score_tab_image = pygame.transform.scale(load_image('score_tab.png'), (110, 42))
+    score_tab_image_rect = score_tab_image.get_rect()
+    score_tab_image_rect.x, score_tab_image_rect.y = \
+        WIDTH // 2 - exit_image_rect.w // 2, HEIGHT // 2 - exit_image_rect.h // \
+        2 + 125 + restart_image_rect.h + exit_image_rect.h + 15
+    # блок
+    block = pygame.sprite.Sprite(end_sprites)
+    block.image = pygame.transform.scale(load_image('static_block.png'), (75, 25))
+    block.rect = block.image.get_rect()
+    block.rect.x, block.rect.y = 290, 450
+    # основной цикл
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if event.pos[0] >= WIDTH // 2 - restart_image_rect.w // 2 + 20 and event.pos[
+                    0] <= WIDTH // 2 - restart_image_rect.w // 2 - 15 + \
+                        restart_image_rect.w and \
+                        event.pos[1] >= HEIGHT // 2 - restart_image_rect.h // 2 + 138 and \
+                        event.pos[
+                            1] <= HEIGHT // 2 - restart_image_rect.h // 2 + 125 + \
+                        restart_image_rect.h:
+                    screen.fill((0, 0, 0))
+                    start_screen()
+                if event.pos[0] >= save_image_rect.x and event.pos[
+                    0] <= save_image_rect.x + save_image_rect.w and event.pos[
+                    1] >= save_image_rect.y and event.pos[
+                    1] <= save_image_rect.y + save_image_rect.h:
+                    save_score()
+                if event.pos[0] >= WIDTH // 2 - exit_image_rect.w // 2 and event.pos[
+                    0] <= WIDTH // 2 - exit_image_rect.w // 2 + exit_image_rect.w and \
+                        HEIGHT // 2 - exit_image_rect.h // 2 + 125 + \
+                        restart_image_rect.h + 5 <= event.pos[
+                    1] <= HEIGHT // 2 - exit_image_rect.h // 2 + 125 + \
+                        restart_image_rect.h + 5 + exit_image_rect.h:
+                    exit()
+                if event.pos[0] >= WIDTH // 2 - exit_image_rect.w // 2 and event.pos[
+                    0] <= WIDTH // 2 - exit_image_rect.w // 2 + score_tab_image_rect.w and \
+                        HEIGHT // 2 - exit_image_rect.h // 2 + 125 + \
+                        restart_image_rect.h + exit_image_rect.h + 15 <= event.pos[
+                    1] <= HEIGHT // 2 - exit_image_rect.h // 2 + 125 + \
+                        restart_image_rect.h + exit_image_rect.h + 15 + score_tab_image_rect.h:
+                    score_screen()
+        end_sprites.draw(screen)
+        end_player.draw(screen)
+        end_player.update()
+        pygame.display.flip()
+        clock.tick(FPS)
+        screen.blit(background, (0, 0))
+        screen.blit(save_image, save_image_rect)
+        screen.blit(text, text_rect)
+        screen.blit(exit_image, exit_image_rect)
+        screen.blit(restart_image, restart_image_rect)
+        screen.blit(game_over_image, image_rect)
+        screen.blit(score_tab_image, score_tab_image_rect)
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
 
 
 class StaticBlock(pygame.sprite.Sprite):
@@ -228,25 +398,33 @@ class Player(pygame.sprite.Sprite):
         self.gravity = GRAVITY
         self.up = True
         self.height = 0
+        self.score = 0
 
     def update(self):
-        if self.up:
-            if self.height < JUMP_HEIGHT:
-                self.height += 1
-                self.rect.y -= 1
-            else:
-                self.up = False
+        if self.rect.y > HEIGHT - self.rect.h + 1:
+            self.kill()
+            game_over_screen(int(self.score / 100))
         else:
-            self.height -= 1
-            self.rect.y += 1
-        if pygame.sprite.spritecollideany(self, static_block) or pygame.sprite.spritecollideany(
-                self, dynamic_block):
-            self.moving_up()
-        if pygame.sprite.spritecollideany(self, wall):
-            if self.rect.x < 300:
-                self.rect.x += 5
+            if self.up:
+                if self.height < JUMP_HEIGHT:
+                    self.height += 1
+                    self.rect.y -= 1
+                else:
+                    self.up = False
             else:
-                self.rect.x -= 5
+                self.height -= 1
+                self.rect.y += 1
+            if pygame.sprite.spritecollideany(self, static_block):
+                self.moving_up()
+                self.score += 100
+            if pygame.sprite.spritecollideany(self, dynamic_block):
+                self.moving_up()
+                self.score += 150
+            if pygame.sprite.spritecollideany(self, wall):
+                if self.rect.x < 300:
+                    self.rect.x += 5
+                else:
+                    self.rect.x -= 5
 
     def moving_up(self):
         if self.up is False:
@@ -261,40 +439,3 @@ class Player(pygame.sprite.Sprite):
 
 
 start_screen()
-running = True
-for i in range(WIDTH // 18):
-    WallLeft((-(35 // 2), 35 * i))
-for i in range(WIDTH // 18):
-    WallRight((WIDTH - (35 // 2), 35 * i))
-for i in range(0, HEIGHT - JUMP_HEIGHT // 2, JUMP_HEIGHT // 2):
-    if i // (JUMP_HEIGHT // 2) == random.randint(1, HEIGHT // (JUMP_HEIGHT // 2)):
-        DynamicBlock(random.randint(25, WIDTH - 85), i)
-    elif i // (JUMP_HEIGHT // 2) == random.randint(1, HEIGHT // (JUMP_HEIGHT // 2)):
-        CrashedBlock(random.randint(25, WIDTH - 85), i)
-    else:
-        StaticBlock(random.randint(25, WIDTH - 85), i)
-first_block_pos_x = random.randint(25, WIDTH - 85)
-StaticBlock(first_block_pos_x, HEIGHT - JUMP_HEIGHT // 2)
-hero = Player((first_block_pos_x, (HEIGHT - JUMP_HEIGHT // 2) - 55))
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-    pressed = pygame.key.get_pressed()
-    if pressed[pygame.K_RIGHT]:
-        hero.right(5)
-    elif pressed[pygame.K_LEFT]:
-        hero.left(5)
-    background = pygame.transform.scale(load_image('background.png'), (WIDTH, HEIGHT))
-    screen.blit(background, (0, 0))
-    wall.draw(screen)
-    static_block.draw(screen)
-    dynamic_block.draw(screen)
-    dynamic_block.update()
-    crashed_block.draw(screen)
-    crashed_block.update()
-    player.draw(screen)
-    player.update()
-    pygame.display.flip()
-    clock.tick(FPS)
-terminate()
